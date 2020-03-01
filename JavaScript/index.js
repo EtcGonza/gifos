3d661b95b17f8562b1540cc5bc65203feebab1fa
EGonza <xetchegaraygonzalo@gmail.com>
2020-03-01 14:57:20 -0300

    Avance con el grid

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,19 +1,43 @@
 const apiKey = "WMgym4yAIPYofgGPrganKNA7n1vg2D5Y";
-const toSearch = 'cats';
+var misTendencias;
 
-console.log('Ejecutando');
+var img = document.createElement('img').src = "/assets/img/photo_2020-02-22_15-52-06.jpg";
 
-barSearch(toSearch);
+var foo = document.getElementById("tendencias");
+foo.appendChild(img);
 
-function barSearch(buscar) {
-    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=${buscar}&api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y`)
-        .then((response) => {
+// document.images.
+// document.getElementById('tendencia').innerHTML('assets/img/photo_2020-02-22_15-52-06.jpg');
+
+// FUNCIONES
+async function getTrending() {
+
+    const trending = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y&limit=4&rating=G`)
+        .then(response => {
+            return response.json();
+        }).then((data) => {
+            return data;
+        }).catch((error) => {
+            console.log('Tuvimos errores pidiendo data: ', error);
+        });
+
+    misTendencias = await (() => {
+        trending.then(res => {
+            return res.data;
+        });
+    });
+
+    console.log(misTendencias);
+}
+
+function barSearch() {
+    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=cats&api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y`)
+        .then(response => {
             return response.json();
         }).then(data => {
-            console.log(data);
             return data;
         })
-        .catch((error) => {
+        .catch(error => {
             return error;
         });
 
