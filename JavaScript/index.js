a794e8b5aa6f3678fa3f6ab21770e4ca32a9fa78
EGonza <xetchegaraygonzalo@gmail.com>
2020-03-23 13:30:48 -0300

    Agregue carga de gifs con la API, y inserto los hastag. Corregi y cambie html y css.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,45 +1,57 @@
 const apiKey = "WMgym4yAIPYofgGPrganKNA7n1vg2D5Y";
-var misTendencias;
 
-var img = document.createElement('img').src = "/assets/img/photo_2020-02-22_15-52-06.jpg";
-
-var foo = document.getElementById("tendencias");
-foo.appendChild(img);
-
-// document.images.
-// document.getElementById('tendencia').innerHTML('assets/img/photo_2020-02-22_15-52-06.jpg');
+loadTrending();
 
 // FUNCIONES
-async function getTrending() {
-
-    const trending = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y&limit=4&rating=G`)
-        .then(response => {
-            return response.json();
-        }).then((data) => {
-            return data;
-        }).catch((error) => {
-            console.log('Tuvimos errores pidiendo data: ', error);
-        });
-
-    misTendencias = await (() => {
-        trending.then(res => {
-            return res.data;
-        });
-    });
-
-    console.log(misTendencias);
+async function getTrending(limitGifs) {
+    const consultaTrending = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y&limit=${limitGifs}&rating=G`);
+    const trendingJson = await consultaTrending.json();
+    return trendingJson.data;
 }
 
-function barSearch() {
-    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=cats&api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y`)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-            return data;
-        })
-        .catch(error => {
-            return error;
-        });
+async function loadTrending() {
+    const nodesImg = document.querySelectorAll('.gif-tendencia img');
+    const nodeParrafos = document.querySelectorAll('.contenedor-hastags');
 
-    return found;
+    var gifs = await getTrending(nodesImg.length);
+
+    gifsHastag = await getTitlesOfGifs();
+
+    // Inserto los GIFS
+    for (contador = 0; contador < nodesImg.length; contador++) {
+        nodesImg[contador].setAttribute('src', gifs[contador].images.downsized.url);
+
+        let createParrafo = document.createElement('p');
+        createParrafo.innerHTML = gifsHastag[contador];
+
+        nodeParrafos[contador].appendChild(createParrafo);
+    }
 }
+
+async function getTitlesOfGifs() {
+    const nodesTrending = document.querySelectorAll('.contenedor-hastags');
+    var gifs = await getTrending(nodesTrending.length);
+    var gifHastag = [];
+
+    for (contador = 0; contador < nodesTrending.length; contador++) {
+        gifHastag[contador] = ` #${gifs[contador].title.replace(/ /g, " #")}`;
+        let deleteIndex = gifHastag[contador].indexOf('#GIF');
+        gifHastag[contador] = gifHastag[contador].slice(0, deleteIndex);
+    }
+    return gifHastag;
+}
+
+
+// function barSearch() {
+//     const found = fetch(`http://api.giphy.com/v1/gifs/search?q=cats&api_key=WMgym4yAIPYofgGPrganKNA7n1vg2D5Y`)
+//         .then(response => {
+//             return response.json();
+//         }).then(data => {
+//             return data;
+//         })
+//         .catch(error => {
+//             return error;
+//         });
+
+//     return found;
+// }
